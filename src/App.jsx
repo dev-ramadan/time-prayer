c15795916461bf04613d6932c4a1e08d7daf9088
dev-ramadan <ganna123adam@gmail.com>
2024-11-11 21:49:32 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try{
-        const response = await fetch(`https://api.aladhan.com/v1/timingsByCity/22-09-2024?city=${city}&country=egypt&method=8`)
+        const response = await fetch(`https://api.aladhan.com/v1/timingsByCity/11-11-2024?city=${city}&country=egypt&method=8`)
         const data_p = await response.json()
         setprayer(data_p.data.timings)
         settime(data_p.data.date.gregorian.date)
@@ -20,6 +20,17 @@ function App() {
     }
     fetchData()
   },[city]);
+
+const format = (time) => {
+  if(!time){
+    return '00:00'
+  }
+  let [hours , munit] = time.split(':').map(Number);
+  const format = hours <= 12 ? 'AM' : 'PM';
+  hours = hours % 12 || 12 ;
+  return `${format} ${hours}:${munit < 10 ? '0' + munit : munit}`;
+}
+
     
   const citys = [
     {names : "القاهرة" , value : "Cairo"},
@@ -33,7 +44,9 @@ function App() {
   return (
     <section>
       <div className="container">
-        <div className="top-sc">
+        <div className="row">
+          <div className="col-12">
+          <div className="top-sc">
           <div className="city">
             <h3>المدينة</h3>
 
@@ -50,11 +63,13 @@ function App() {
             <h3>{time}</h3>
           </div>
         </div>
-        <Prayer name="الفجر" time= {prayer.Fajr}/>
-        <Prayer name="الظهر" time={prayer.Dhuhr}/>
-        <Prayer name="العصر" time={prayer.Asr}/>
-        <Prayer name="المغرب" time={prayer.Maghrib}/>
-        <Prayer name="العشاء" time={prayer.Isha}/>
+        <Prayer name="الفجر" time= {format(prayer.Fajr)}/>
+        <Prayer name="الظهر" time={ format(prayer.Dhuhr)}/>
+        <Prayer name="العصر" time={format(prayer.Asr)}/>
+        <Prayer name="المغرب" time={format(prayer.Maghrib)}/>
+        <Prayer name="العشاء" time={format(prayer.Isha)}/>
+          </div>
+        </div>
       </div>
     </section>
 
